03786bea2ff20828be2fd022f761c6ca6388ac27
AdityaKashyap <aditya24kashyap@gmail.com>
2026-05-29 18:30:52 +0530

    fix webcontainer headers

--- next.config.ts
+++ next.config.ts
@@ -5,18 +5,35 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 
+  reactStrictMode: false,
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*",
-        port: "",
-        pathname: "/**",
       },
     ],
   },
 
-  reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        // ONLY apply to playground
+        source: "/playground/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
